9311f1aa08e99c875a29f3101e18f60004b6af98
Thomas Solenthaler <tsolenthaler@gmail.com>
2023-05-24 10:03:58 +0200

    fix transform Akeneo DTO

--- src/transform.py
+++ src/transform.py
@@ -83,72 +83,73 @@ def setValue(data, locale, scope):
         dataValue.append(frValue)
   return dataValue
 
-def setValue(data, locale = None, scope = None):
-  if type(data) is str:
-    dataValue = []
-    value = {}
-    value['data'] = data
-    value['locale'] = locale
-    value['scope'] = scope
-    dataValue.append(value)
-    return dataValue
-  elif type(data) is int:
-    dataValue = []
-    value = {}
-    value['data'] = data
-    value['locale'] = locale
-    value['scope'] = scope
-    dataValue.append(value)
-    return dataValue
-  elif type(data) is dict:
-    dataValue = []
-    for key, value in data.items():
-      print(key)
-      print(value)
-      if key == 'de':
-        deValue = {}
-        deValue['data'] = value
-        deValue['locale'] = 'de_CH'
-        deValue['scope'] = scope
-        dataValue.append(deValue)
-      elif key == 'en':
-        enValue = {}
-        enValue['data'] = value
-        enValue['locale'] = 'en_US'
-        enValue['scope'] = scope
-        dataValue.append(enValue)
-      elif key == 'fr':
-        frValue = {}
-        frValue['data'] = value
-        frValue['locale'] = 'fr_FR'
-        frValue['scope'] = scope
-        dataValue.append(frValue)
-    return dataValue
-  
+def setValueStr(data, locale, scope):
+  dataValue = []
+  value = {}
+  value['data'] = data
+  value['locale'] = locale
+  value['scope'] = scope
+  dataValue.append(value)
+  return dataValue
+
+def setValueInt(data, locale, scope):
+  dataValue = []
+  value = {}
+  value['data'] = data
+  value['locale'] = locale
+  value['scope'] = scope
+  dataValue.append(value)
+  return dataValue
+
 def setValueDict(data, locale = None, scope = None):
   dataValue = []
   for key, value in data.items():
       print(key)
       print(value)
-      if key == 'de':
-        deValue = {}
-        deValue['data'] = value
-        deValue['locale'] = 'de_CH'
-        deValue['scope'] = scope
-        dataValue.append(deValue)
-      elif key == 'en':
-        enValue = {}
-        enValue['data'] = value
-        enValue['locale'] = 'en_US'
-        enValue['scope'] = scope
-        dataValue.append(enValue)
-      elif key == 'fr':
-        frValue = {}
-        frValue['data'] = value
-        frValue['locale'] = 'fr_FR'
-        frValue['scope'] = scope
-        dataValue.append(frValue)
+      if locale is not None:
+        if key == 'de':
+          deValue = {}
+          deValue['data'] = value
+          deValue['locale'] = 'de_CH'
+          deValue['scope'] = scope
+          dataValue.append(deValue)
+        elif key == 'en':
+          enValue = {}
+          enValue['data'] = value
+          enValue['locale'] = 'en_US'
+          enValue['scope'] = scope
+          dataValue.append(enValue)
+        elif key == 'fr':
+          frValue = {}
+          frValue['data'] = value
+          frValue['locale'] = 'fr_FR'
+          frValue['scope'] = scope
+          dataValue.append(frValue)
+      else:
+        newValue = {}
+        newValue['data'] = data
+        newValue['locale'] = locale
+        newValue['scope'] = scope
+        dataValue.append(newValue)
   return dataValue
+
+def setValueList(data, locale = None, scope = None):
+  dataValue = []
+  print("LIST Type")
+  for key, value in data.items():
+      print(key)
+      print(value)
+
+
+def setValue(data, locale = None, scope = None):
+  if type(data) is str:
+    return setValueStr(data, locale, scope)
+  elif type(data) is int:
+    return setValueInt(data, locale, scope)
+  elif type(data) is dict:
+    return setValueDict(data, locale, scope)
+  elif type(data) is list:
+    return setValueList(data, locale, scope)
 
 def getFieldsValuebyKey(key, data):
   for field in data['fields']:
@@ -162,6 +163,12 @@ def getFieldsValuebyKey(key, data):
         print (type(field["field_value"]))
         print (field["field_name"])
         return field["field_value"]
+
+def transformFieldtoAkeneoAttribut(maschProperty, maschData, local, scope):
+  field = getFieldsValuebyKey(maschProperty, maschData)
+  if field:
+      fieldValue = setValue(field['de'], local, scope)
+      return fieldValue  
 
 def transform(data, indexAkeneo):
   transformData = []
@@ -184,33 +191,40 @@ def transform(data, indexAkeneo):
     # name
     importProduct['values']['name'] = []
     name = getFieldsValuebyKey('teaser_title_hotel_name', item)
-    nameValue = setValue(name, 'de_CH')
-    importProduct['values']['name'] = nameValue
+    if name:
+      nameValue = setValue(name['de'], 'de_CH')
+      importProduct['values']['name'] = nameValue
     # description
     importProduct['values']['description'] = []
     description = getFieldsValuebyKey('blog_table_description', item)
-    descriptionValue = setValue(description, 'de_CH', 'ecommerce')
-    importProduct['values']['description'] = descriptionValue
+    if description:
+      descriptionValue = setValue(description['de'], 'de_CH', 'ecommerce')
+      importProduct['values']['description'] = descriptionValue
     # url
-    importProduct['values']['url'] = []
-    url = getFieldsValuebyKey('metaserver_hotel_website', item)
-    urlValue = setValue(url, None, 'ecommerce')
-    importProduct['values']['url'] = urlValue
+    # Locale : None, spezifisch/default (bspw. de_CH) or All
+    # Scope : None, spezifisch/default (bspw. ecommerce) or All
+    importProduct['values']['url'] = transformFieldtoAkeneoAttribut('metaserver_hotel_website', item, None, None)
     # email
-    importProduct['values']['email'] = []
-    email = getFieldsValuebyKey('blog_table_contact_email', item)
-    emailValue = setValue(email, None, 'ecommerce')
-    importProduct['values']['email'] = emailValue
+    importProduct['values']['email'] = transformFieldtoAkeneoAttribut('blog_table_contact_email', item, None, None)
     # telephone
-    importProduct['values']['telephone'] = []
-    telephone = getFieldsValuebyKey('blog_table_contact_phone', item)
-    telephoneValue = setValue(telephone, None, 'ecommerce')
-    importProduct['values']['telephone'] = telephoneValue
-    # addressLocality
-    importProduct['values']['addressLocality'] = []
-    addressLocality = getFieldsValuebyKey('blog_table_address_locality', item)
-    addressLocalityValue = setValue(addressLocality, 'de_CH', 'ecommerce')
-    importProduct['values']['addressLocality'] = addressLocalityValue
+    # Not in MASCH
+    #importProduct['values']['telephone'] = []
+    #telephone = getFieldsValuebyKey('blog_table_contact_phone', item)
+    #telephoneValue = setValue(telephone['de'], None, 'ecommerce')
+    #importProduct['values']['telephone'] = telephoneValue
+    # addressLocality / teaser_title_hotel_place - Alternative: metaserver_city
+    importProduct['values']['addressLocality'] = transformFieldtoAkeneoAttribut('teaser_title_hotel_place', item, None, None)
+    # streetAddress / metaserver_address
+    importProduct['values']['streetAddress'] = transformFieldtoAkeneoAttribut('metaserver_address', item, None, None)
+    # addressCountry / metaserver_country
+    importProduct['values']['addressCountry'] = transformFieldtoAkeneoAttribut('metaserver_country', item, None, None)
+    # Geo
+    # latitude / blog_seo_latitude
+    importProduct['values']['latitude'] = transformFieldtoAkeneoAttribut('blog_seo_latitude', item, None, None)
+    # longitude / blog_seo_longitude
+    importProduct['values']['longitude'] = transformFieldtoAkeneoAttribut('blog_seo_longitude', item, None, None)
+    # blog_table_video_url_link
+    # teaser_video_id
 
     transformData.append(importProduct)
   return transformData
