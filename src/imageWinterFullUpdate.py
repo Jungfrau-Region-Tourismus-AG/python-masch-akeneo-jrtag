6076eb804df8f7dcf3a1caa2a4cf6156192c0439
Thomas Solenthaler <tsolenthaler@gmail.com>
2023-12-04 10:28:07 +0100

    Add imageSummerFullUpdate and imageWinterFullUpdate scripts

---
+++ src/imageWinterFullUpdate.py
@@ -0,0 +1,30 @@
+from extract import extract, getAkeneoProducts
+from transform import transformImage, transformAkeneotoMasch
+from load import downloadImages,loadImages
+
+def __main__():
+  print("STARTING")
+  print("EXTRACTING")
+  extractData = extract()
+  extractDataAkeneo = getAkeneoProducts()
+  
+  print("TRANSFORMING")
+  transformDataAkeneo = transformAkeneotoMasch(extractDataAkeneo)
+  transformImageData = transformImage(extractData, transformDataAkeneo, 'teaser_and_content_banner_picture_winter', 'image_winter')
+  #transformImageDataSummer = transformImage(extractData, transformDataAkeneo, 'teaser_and_content_banner_picture_summer', 'image_summer')
+  #transformImageDataWinter = transformImage(extractData, transformDataAkeneo, 'teaser_and_content_banner_picture_winter', 'image_winter')
+  
+  print("LOADING")
+  #loadData = load(transformData)
+  print("DOWNLOAD IMAGES")
+  downloadFiles = downloadImages(transformImageData)
+  #downloadFilesSummer = downloadImages(transformImageDataSummer)
+  #downloadFilesWinter = downloadImages(transformImageDataWinter)
+  print("LOAD IMAGES")
+  loadData = loadImages(transformImageData)
+  #loadDataSummer = loadImages(transformImageDataSummer)
+  #loadDataWinter = loadImages(transformImageDataWinter)
+  print("DONE")
+
+if __name__== "__main__":
+    __main__()
