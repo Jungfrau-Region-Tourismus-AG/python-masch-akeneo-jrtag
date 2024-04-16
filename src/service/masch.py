d859ef41fef4d1977d8e1119efb70fe1be727d64
Thomas Solenthaler <tsolenthaler@gmail.com>
2024-04-16 08:59:12 +0200

    Refactor code in masch.py

--- src/service/masch.py
+++ src/service/masch.py
@@ -89,7 +89,7 @@ def postObjecttoMasch(product):
     headers = {'Content-Type': 'application/json'}
     r = requests.post(url, json=product, headers=headers)
     print(r.status_code)
-    print(r.text)
+    #print(r.text)
     #print(json.decoder(r.json()))
     return r.status_code
 
