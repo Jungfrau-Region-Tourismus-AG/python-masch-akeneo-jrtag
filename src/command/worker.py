3db42d4e35ed75272a47b7bc05b4650eac7ff511
Thomas Solenthaler <tsolenthaler@gmail.com>
2024-12-06 08:45:35 +0100

    Refactor worker script to remove old functionality and streamline MASCH update process

--- src/command/worker.py
+++ src/command/worker.py
@@ -1,39 +1,13 @@
-# Update to MASCH
+# Update to MASCH all 5 minutes
 import json
 import sys
 sys.path.append("..")
 
-from service.objectStorage import getObject, getObjects, putObject, countFilesInFolder, folderExist
-from service.masch import getMaschUpdateJobs, checkProductsMasch, transformAkeneotoMasch, loadObjectstoMasch, postImagestoMasch
-
-def debugJSONfile(transfromDataMASCH):
-    # Add transformDataMASCH to json file in output folder
-    print("WRITING to output/transformDataMasch.json")
-    with open("../../output/transformDataMasch.json", "w") as file:
-        file.write(json.dumps(transfromDataMASCH, indent=4))
-
 def __main__():
     print("STARTING - WORKER")
-    print("GET MASCH UPDATES")
-    updateList = getMaschUpdateJobs()
-    print(updateList)
-    productList = getObjects(updateList)
-
-    print("TRANSFORMING from Akeneo to MASCH")
-    #print (productList)
-    transformDataMASCH = transformAkeneotoMasch(productList)
-
-    #debugJSONfile(transformDataMASCH)
-  
-    print("LOADING to MASCH")
-    loadObjectstoMasch(transformDataMASCH)
-    print("POSTING IMAGES to MASCH")
-    postImagestoMasch(productList)
-
-    print ("CLEAR MASCH UPDATE LIST")
-    updateList = {}
-    putObject(updateList, 'export/contentdesk/job/masch/updates/index.json')
-    print("DONE")
-
+    # 1. Check last changes all Products with maschId in Akeneo
+    
+    print("END - WORKER")
+    
 if __name__== "__main__":
     __main__()
