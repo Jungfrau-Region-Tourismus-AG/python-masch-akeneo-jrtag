94220555a568b7ec280486931ca14eab860a01e3
Thomas Solenthaler <tsolenthaler@gmail.com>
2024-12-06 11:54:35 +0100

    Refactor worker script to modularize contentdesk product retrieval and filtering by datetime

--- src/command/worker.py
+++ src/command/worker.py
@@ -10,22 +10,12 @@ from service.loadEnv import loadEnv
 import service.debug as debug
 from service.objectStorage import getObject, getObjects, putObject, countFilesInFolder, folderExist
 
-def maschFlow():
-    print ("Masch Flow")
-    maschRecords = getMaschPull()
-    print(maschRecords)
-    if maschRecords['result'] == 'success':
-        if len(maschRecords['records']) > 0:
-            # Update to Contentdesk
-            print("UPDATE to Contentdesk")
-            # TODO: Update to Contentdesk
-        else:
-            print("No new records.")
-            
-def contentdeskFlow():
-    print ("Contentdesk Flow")
-    # CHECK
-    targetCon = loadEnv('ziggy')
+from extract import extract, getAkeneoProducts
+from transform import transform, transformAkeneotoMasch
+from load import load
+
+def getContentdeskUpdatedProducts(env):
+    targetCon = loadEnv(env)
     target = Akeneo(targetCon["host"], targetCon["clientId"], targetCon["secret"], targetCon["user"], targetCon["passwd"])
     search = '{"labels":[{"operator":"NOT EMPTY","value":""}]}&attributes=labels'
     end_time = datetime.datetime.now()
@@ -36,22 +26,63 @@ def contentdeskFlow():
     search = '{"maschId":[{"operator":"NOT EMPTY","value":""}],"maschUpdated":[{"operator":"BETWEEN","value":["' + startDayStr + '","' + endDayStr + '"]}]}'
     print(search)
     contentdeskRecords = target.getProductBySearch(search)
-    debug.addToFileFull("worker", "ziggy", "export", "maschId", "extractProducts", contentdeskRecords)
-    
-    # Filter by datetime
+    return contentdeskRecords
+
+def getContentdeskProducts(env):
+    targetCon = loadEnv(env)
+    target = Akeneo(targetCon["host"], targetCon["clientId"], targetCon["secret"], targetCon["user"], targetCon["passwd"])
+    search = '{"maschId":[{"operator":"NOT EMPTY","value":""}]}'
+    products = target.getProducts(limit=100, search=search )
+    return products
+
+def checkContentdeskProductsbyDatetime(products):
     recentRecords = []
-    for record in contentdeskRecords:
+    for record in products:
         # string to datetime
+        end_time = datetime.datetime.now()
+        start_time = end_time - datetime.timedelta(minutes=5)
         maschUpdatedStr = record['values']['maschUpdated'][0]['data']
         maschUpdated = datetime.datetime.fromisoformat(maschUpdatedStr)
         maschUpdated = maschUpdated.strftime('%Y-%m-%d %H:%M:%S')
         startDayTime = start_time.strftime('%Y-%m-%d %H:%M:%S')
         endDayTime = end_time.strftime('%Y-%m-%d %H:%M:%S')
-        print("Compare maschUpdated")
         if startDayTime <= maschUpdated <= endDayTime:
-            print("OK")
+            print("Add record to Update")
             recentRecords.append(record)
-    debug.addToFileFull("worker", "ziggy", "export", "maschId", "filterbyDatetimeProducts", recentRecords)
+    return recentRecords
+
+def maschFlow():
+    print ("Masch Flow")
+    maschRecords = getMaschPull()
+    print(maschRecords)
+    debug.addToFileFull("worker", "ziggy", "export", "maschId", "extractObjectsMasch", maschRecords)
+    
+    if maschRecords['result'] == 'success':
+        if len(maschRecords['records']) > 0:
+            # Update to Contentdesk
+            print("START - UPDATE to Contentdesk")
+            # TODO: Update to Contentdesk
+            extractDataAkeneo = getContentdeskProducts("ziggy")
+            print("TRANSFORMING to Contentdesk")
+            transformDataAkeneo = transformAkeneotoMasch(extractDataAkeneo)
+            transformData = transform(maschRecords, transformDataAkeneo)
+            debug.addToFileFull("worker", "ziggy", "export", "maschId", "transformDataMasch", transformData)
+            print("LOADING to Contentdesk")
+            loadData = load(transformData)
+            debug.addToFileFull("worker", "ziggy", "export", "maschId", "loadObjectstoMasch", loadData)
+            print("DONE")
+        else:
+            print("No new records.")
+            
+def contentdeskFlow():
+    print ("Contentdesk Flow")
+    # CHECK
+    contentdeskRecords = getContentdeskUpdatedProducts("ziggy")
+    debug.addToFileFull("worker", "ziggy", "export", "maschId", "extractProductsContentdesk", contentdeskRecords)
+    
+    # FILTER by datetime  
+    recentRecords = checkContentdeskProductsbyDatetime(contentdeskRecords)
+    debug.addToFileFull("worker", "ziggy", "export", "maschId", "filterbyDatetimeProductsContentdesk", recentRecords)
     
     # Update to MASCH
 
