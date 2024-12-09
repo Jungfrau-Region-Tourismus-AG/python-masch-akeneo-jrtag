5212d988ad95c45fe3182877f4cbe348c71296a9
Thomas Solenthaler <tsolenthaler@gmail.com>
2024-12-09 13:08:20 +0100

    Remove maschFlow function and related logging; streamline masch.py for improved clarity and maintainability

--- src/service/masch.py
+++ src/service/masch.py
@@ -285,30 +285,3 @@ def loadObjectstoMasch(products):
     print("DONE")
     return response
 
-def maschFlow():
-    print (" - START: Masch Flow")
-    maschRecords = getMaschPull()
-    print(maschRecords)
-    debug.addToFileFull("worker", "ziggy", "export", "maschId", "extractObjectsMasch", maschRecords)
-    
-    if maschRecords['result'] == 'success':
-        if len(maschRecords['records']) > 0:
-            # Update to Contentdesk
-            print("   - START - UPDATE to Contentdesk")
-            extractDataAkeneo = contentdesk.getContentdeskProducts()
-            debug.addToFileFull("worker", "ziggy", "export", "maschId", "extractDataAkeneo", extractDataAkeneo)
-            
-            print("   - TRANSFORMING to Contentdesk")
-            transformData = transform(maschRecords['records'], extractDataAkeneo)
-            debug.addToFileFull("worker", "ziggy", "export", "maschId", "transformDataAkeneo", transformData)
-            
-            print("   - LOAD - Update to Contentdesk")
-            #TODO: Implement load function
-            #loadData = load(transformData)
-            #debug.addToFileFull("worker", "ziggy", "export", "maschId", "loadData", loadData)
-            
-            print("   - DONE - UPDATE to Contentdesk")
-        else:
-            print("   - No new records.")
-            
-    print(" - DONE: Masch Flow")
