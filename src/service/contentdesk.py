e2ae083560b1a3fe122eb3d53a86f7b78c103345
Thomas Solenthaler <tsolenthaler@gmail.com>
2024-12-09 12:38:58 +0100

    Refactor checkContentdeskProductsbyDatetime function to enhance date handling and logging; streamline comparison logic for updated records

--- src/service/contentdesk.py
+++ src/service/contentdesk.py
@@ -49,42 +49,53 @@ def updateContentdeskProducts(products):
             "values": {
                 "maschUpdated": [
                     {
+                        "locale": None,
+                        "scope": None,
                         "data": datetime.datetime.now().isoformat()
                     }
                 ]
             }
         }
-        target.patchProductByCode(item['identifier'], body)
+        response = target.patchProductByCode(item['identifier'], body)
+        print(response)
+        
 
 def checkContentdeskProductsbyDatetime(products):
     recentRecords = []
     for item in products:
         # string to datetime
-        print("    - Check Product: "+item['identifier'])
+        print("    * CHECK Product: "+item['identifier'])
         end_time = datetime.datetime.now()
         start_time = end_time - datetime.timedelta(minutes=5)
         updatedDateStr = item['updated']
         updatedDate = datetime.datetime.fromisoformat(updatedDateStr)
-        updatedDate = updatedDate.strftime('%Y-%m-%d %H:%M:%S')
+        updatedDate = updatedDate.strftime('%Y-%m-%d %H:%M')
         
         maschUpdatedStr = item['values']['maschUpdated'][0]['data']
         maschUpdated = datetime.datetime.fromisoformat(maschUpdatedStr)
-        maschUpdated = maschUpdated.strftime('%Y-%m-%d %H:%M:%S')
+        maschUpdated = maschUpdated.strftime('%Y-%m-%d %H:%M')
 
-        startDayTime = start_time.strftime('%Y-%m-%d %H:%M:%S')
-        endDayTime = end_time.strftime('%Y-%m-%d %H:%M:%S')
-        print ("    - Start Time: " + startDayTime + " - End Time: " + endDayTime)
-        print ("    - Updated: " + updatedDate)
-        print ("    - Masch Updated: " + maschUpdated)
+        startDayTime = start_time.strftime('%Y-%m-%d %H:%M')
+        endDayTime = end_time.strftime('%Y-%m-%d %H:%M')
         
-        if updatedDate >= start_time.strftime('%Y-%m-%d %H:%M:%S') and updatedDate <= end_time.strftime('%Y-%m-%d %H:%M:%S'):
+        if updatedDate >= start_time.strftime('%Y-%m-%d %H:%M') and updatedDate <= end_time.strftime('%Y-%m-%d %H:%M'):
             print("     - Item Updated in last 5min")
-        
-        if startDayTime <= updatedDate <= endDayTime:
-            print("     - Add record to Update")
+            print ("    - COMPARE")
+            print ("    - Start Time: " + startDayTime + " - End Time: " + endDayTime)
+            print ("    - Updated: " + updatedDate)
+            print ("    - Masch Updated: " + maschUpdated)
             if updatedDate != maschUpdated:
-                print("     - Updated Date is not equal to Masch Updated Date")
-                recentRecords.append(item)
+                if startDayTime <= updatedDate <= endDayTime:
+                    print("     - Add record to Update")
+                    if updatedDate != maschUpdated:
+                        print("     - Updated Date is not equal to Masch Updated Date")
+                        recentRecords.append(item)
+                    else:
+                        print("     - Updated Date is equal to Masch Updated Date")
+                else:
+                    print("     - Updated Date is not in the last 5min")
+            else:
+                print("     - Updated Date is equal to Masch Updated Date")
     return recentRecords
 
 def contentdeskFlow():
@@ -101,22 +112,21 @@ def contentdeskFlow():
     
     if len(recentRecords) == 0:
         print("   - No new records to update.")
-        return
+    else:
+        # Transform to MASCH
+        print("   - Transform to MASCH")
+        transformDataMASCH = masch.transformAkeneotoMasch(recentRecords)
+        debug.addToFileFull("worker", env, "export", "maschId", "transformDataMASCH", transformDataMASCH)
+        
+        # Update to MASCH
+        print("   - Update to MASCH")
+        #masch.loadObjectstoMasch(transformDataMASCH)
+        
+        ## Update Images to MASCH - Not needed
+        print("   - POSTING IMAGES to MASCH")
+        #postImagestoMasch(productList)
     
-    # Transform to MASCH
-    print("   - Transform to MASCH")
-    transformDataMASCH = masch.transformAkeneotoMasch(recentRecords)
-    debug.addToFileFull("worker", env, "export", "maschId", "transformDataMASCH", transformDataMASCH)
-    
-    # Update to MASCH
-    print("   - Update to MASCH")
-    #masch.loadObjectstoMasch(transformDataMASCH)
-    
-    ## Update Images to MASCH - Not needed
-    print("   - POSTING IMAGES to MASCH")
-    #postImagestoMasch(productList)
-    
-    # Update Contentesk Attribute MaschUpdated
-    updateContentdeskProducts(recentRecords)
+        # Update Contentesk Attribute MaschUpdated
+        updateContentdeskProducts(recentRecords)
     
     print(" - DONE: Contentdesk Flow")
